8f16784fa9431e09a48535f23c0c621ea1fd4a99
Zachary Kubli <zkubli@e1z2r3p7.42.us.org>
2020-02-16 21:44:43 -0800

    all my stuff

--- ex00/hash_table.c
+++ ex00/hash_table.c
@@ -6,24 +6,25 @@
 /*   By: zkubli <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 01:49:43 by zkubli            #+#    #+#             */
-/*   Updated: 2020/02/16 03:00:14 by zkubli           ###   ########.fr       */
+/*   Updated: 2020/02/16 21:14:12 by zkubli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "archive.h"
 #include <stdlib.h>
+#include <string.h>
 
 /*
 ** Frees one row of table, doesn't set anything to zero
 */
 
-static void clear_chain(t_hashnode *table)
+static void	clear_chain(t_hashnode *table)
 {
 	if (table)
 	{
 		clear_chain(table->next);
 		free(table);
-	}	
+	}
 }
 
 void		table_free(t_hashnode **table)
@@ -49,7 +50,7 @@ char		*table_checkadd(t_hashnode **table, ino_t my_ino, char *s)
 	{
 		if ((*tmp)->my_ino == my_ino)
 			return ((*tmp)->my_name);
-		(*tmp) = &(*tmp)->next;
+		tmp = &(*tmp)->next;
 	}
 	(*tmp) = malloc(sizeof(t_hashnode));
 	(*tmp)->my_ino = my_ino;
